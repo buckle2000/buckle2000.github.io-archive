1a3bbc14bda1d43161b770020990e4ea781cef5e
buckle2000 <buckle2000@zoho.com>
2017-06-11 00:22:11 +0800

    Change 'my works' to 'edu' section

--- _serve.bat
+++
@@ -1,2 +0,0 @@
-start /min jekyll serve --draft
-start "" http://127.0.0.1:4000/
---
+++ _serve.cmd
@@ -0,0 +1,2 @@
+start /min jekyll s
+start /min browser-sync start --proxy localhost:4000 --port 3001 --files _site --open local
